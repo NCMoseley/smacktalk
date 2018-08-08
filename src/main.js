49405ad5ac9fceb2bf964ba311678bebde5f263b
NCMoseley <ncmoseley@gmail.com>
2018-08-08 11:30:38 -0700

    Add auth.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable*/
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 import router from './router'
-import Vuefire from 'vuefire'
+import firebase from './firebase/index'
 
 Vue.config.productionTip = false
-Vue.use(Vuefire)
+
+Vue.use(VueRouter)
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$router.push('/chat')
+      } else {
+        this.$router.push('/auth')
+      }
+    })
+  },
+  el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
+/*eslint-disable*/
+
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Auth from '../components/Auth.vue'
+import Chat from '../components/Chat.vue'
 
-Vue.use(Router)
-
-export default new Router({
+const router = new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/', component: Auth },
+    { path: '/auth', component: Auth },
+    { path: '/chat', component: Chat }
   ]
 })
+
+export default router
